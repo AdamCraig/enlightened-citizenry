19f0443357ae50ccdca8f30085cfea5ec333c4f2
Adam Craig and Luca Quatela <adam+luca@>
2016-06-14 11:39:24 -0700

    Successfully implement two api calls in one model using RSVP.hash. Loop through both in the corresponding .hbs file to display two separate lists of committees by category

--- app/routes/committee-results.js
+++ app/routes/committee-results.js
@@ -1,29 +1,18 @@
 import Ember from 'ember';
 
+var urlHouse = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&apikey=ab3a673b4f5e4db09dad0ec880a7ac8a';
+var urlSenate = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&apikey=ab3a673b4f5e4db09dad0ec880a7ac8a';
+
 export default Ember.Route.extend({
   model: function() {
-    var urlHouse = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&apikey=ab3a673b4f5e4db09dad0ec880a7ac8a';
-    var urlSenate = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&apikey=ab3a673b4f5e4db09dad0ec880a7ac8a';
-
-    var houseCommitteesList = Ember.$.getJSON(urlHouse).then(function(responseJSON){
-      return responseJSON.results;
+    return Ember.RSVP.hash({
+      house: Ember.$.getJSON(urlHouse).then(function(responseJSON){
+        return responseJSON.results;
+      }),
+      senate: Ember.$.getJSON(urlSenate).then(function(responseJSON){
+        return responseJSON.results;
+      }),
     });
-
-    var senateCommitteesList = Ember.$.getJSON(urlSenate).then(function(responseJSON){
-      return responseJSON.results;
-    });
-
-    console.log(houseCommitteesList);
-    console.log(senateCommitteesList);
-
-    var bothCommitteesList = {
-      senate: senateCommitteesList,
-      house: houseCommitteesList,
-    };
-
-    console.log(bothCommitteesList);
-
-    return bothCommitteesList;
   },
 
 });
